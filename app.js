5fe22d7282e0467c67ec3db12383b868f3fa27df
chadcates <cates.chad@gmail.com>
2015-09-02 15:55:46 -0500

    uninstalled sqlite3 and installed pg

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-var sqlite3 = require('sqlite3');
-var db = new sqlite3.Database('./Northwind.sl3');
+var pg = require('pg');
+var conString = "postgres://localhost/Northwind";
+//this initializes a connection pool
+//it will keep idle connections open for a (configurable) 30 seconds
+//and set a limit of 20 (also configurable)
+pg.connect(conString, function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT * FROM Categories', function(err, result) {
+    //call `done()` to release the client back to the pool
+    done();
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows);
+    client.end();
+    //output: 1
+  });
+});
+
 
 db.serialize(function () {
   db.run('', function () {
